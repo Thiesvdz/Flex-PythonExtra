8906b85221507886f80ec683c075bd12046c851a
Thies Van der zon <71008930+Thiesvdz@users.noreply.github.com>
2020-10-29 16:41:11 +0100

    Delete Obama.py

--- 03-MemesGifs/Obama.py
+++
@@ -1,9 +0,0 @@
-Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-========== RESTART: C:/Flex-PythonExtra/03-MemesGifs/flexafbeelding.py =========
->>> 
-========== RESTART: C:/Flex-PythonExtra/03-MemesGifs/flexafbeelding.py =========
-De afbeelding is 2687 pixels breed en 3356 pixels hoog
-JPEG (2687, 3356) RGB
->>> 
