7196bade05ed6bcf613bf073607f4732d1f4b1bc
Thies Van der zon <71008930+Thiesvdz@users.noreply.github.com>
2020-11-18 11:15:28 +0100

    Add files via upload

---
+++ 01-Turtle/turtletekening3.py
@@ -0,0 +1,12 @@
+import turtle 
+
+turtle.speed(20)
+
+for x in range(180):
+    turtle.forward(350)
+    turtle.right(20)
+    turtle.forward(350)
+    turtle.setposition(0, 0)
+    turtle.right(5)
+    
+turtle.done()
